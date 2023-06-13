df46e3b5b454ca0c93c05c33deddf222711675ba
Fernando Ramirez <86069304+ferrami@users.noreply.github.com>
2023-06-13 02:32:58 +0000

    button to add note added and working, missing delete button ( In Progress )

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [inputValue, setInputValue] = useState('');
+	const [items, setItems] = useState([]);
+	
+	const addTask = () => {
+		if ( inputValue != ''){
+			setItems([...items, inputValue]);
+			setInputValue('');
+		}
+	}
+
+	const deleteTask = () => {
+
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container">
+			<h1 className="text-center mt-5">To Do List</h1>
+			
+			<ul className="list-group">
+				<li className="list-group-item pe-2 d-flex justify-content-start">
+					<input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+					<button className="btn btn-success mx-2" onClick={addTask} >Add Task</button>
+					{/* <button className="btn btn-success">Delete Task</button> */}
+				</li>
+				<ul className="list-group">
+					{items.map((item, index) => (
+						<li className="list-group-item d-flex justify-content-start" key={index}>{item}</li>
+					))} 
+				</ul>
+				
+			</ul>
+			
 		</div>
 	);
 };
