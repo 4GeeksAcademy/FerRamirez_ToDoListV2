ef44bd877e17acb6225715f127bc8653d565704d
Fernando Ramirez <86069304+ferrami@users.noreply.github.com>
2023-06-28 18:50:45 +0000

    Done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,130 +1,110 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
-
-	const [inputValue, setInputValue] = useState({ label: "", done: false });
-	const [items, setItems] = useState([""]);
-	const [count, setCount] = useState(0);
-
-	const postData = async () => {
-
-		try {
-			const username = `ferrami`;
-			const url = `https://assets.breatheco.de/apis/fake/todos/user/${username}`;
-
-			const response = await fetch(url, {
-				method: 'PUT',
-				headers: {
-					'Content-type': 'application/json'
-				},
-				body: JSON.stringify(items)
-			});
-
-			if (!response.ok) {
-				throw new Error('Error');
-			}
-
-			const result = await response.json();
-			console.log('Result:', result);
-		} catch (error) {
-			console.log('Error:', error)
-		}
-	};
+	const [inputValue, setInputValue] = useState("");
+	const [todoList, settodoList] = useState([]);
 
 	const createUser = async () => {
-
-		try {
-
-			const url = `https://assets.breatheco.de/apis/fake/todos/user/ferrami`;
-			const response = await fetch(url, {
+		const response = await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/fernando",
+			{
 				method: "POST",
+				body: JSON.stringify([]),
 				headers: {
-					'Content-Type': 'application/json'
+					"Content-Type": "application/json",
 				},
-				body: JSON.stringify([])
-			});
-			if (!response.ok) {
-				throw new Error('Error request');
 			}
-			const data = await response.json();
-			console.log('Respuesta', data);
-		} catch (error) {
-			console.log('Error al crear usuario, ya existe :', error);
-		}
+		);
+		const data = await response.json();
+		console.log(data);
 	};
 
-	const getData = async () => {
+	const gettodoList = async () => {
 		try {
 			const response = await fetch(
-				"https://assets.breatheco.de/apis/fake/todos/user/ferrami"
+				"https://assets.breatheco.de/apis/fake/todos/user/fernando"
 			);
-
 			const data = await response.json();
-			setItems(data);
+			settodoList(data);
 		} catch (error) {
 			console.log(error);
 		}
-
-		useEffect(() => {
-			if (!createUser) {
-				createUser();
-			}
-			getData();
-		}, [])
-
-		useEffect(() => {
-			postData();
-		}, [items])
 	};
 
-	const addTask = async (e) => {
-		if (e.key === "Enter" && inputValue.trim() !== "") {
-			setItems([...items, { label: inputValue, done: true }]);
-			setInputValue({ label: "", done: false });
-		} if ((e.key === "Enter" && inputValue.trim() == "") || (e.type === "click" && inputValue.label == "")) {
-			alert("Alert")
+	const UpdatetodoList = async () => {
+		try {
+			const response = await fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/fernando",
+				{
+					method: "PUT",
+					body: JSON.stringify(todoList),
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+			const data = await response.json();
+			console.log(response.ok); // will be true if the response is successfull
+			console.log(response.status); // the status code = 200 or code = 400 etc.
+			console.log(data);
+		} catch (error) {
+			console.log(error);
 		}
-	}
+	};
 
 	useEffect(() => {
-		setCount(items.length);
-	}, [items]);
-
-	const deleteItems = async (index) => {
-		setItems((pItems) => pItems.filter((_, i) => i !== index))
-		if (items.length == 0) {
-			handleRemoveItem()
+		if (!createUser) {
+			createUser();
 		}
-	}
+	}, []);
+	useEffect(() => {
+		gettodoList();
+	}, []);
+	useEffect(() => {
+		UpdatetodoList();
+	}, [todoList]);
 
-	const handleRemoveItem = async () => {
-		const newArray = myArray.slice(0, -1);
-		setItems(newArray);
-	}
+	const addTask = (e) => {
+		if (e.key === "Enter" && inputValue.trim() !== "") {
+			settodoList([...todoList, { label: inputValue, done: false }]);
+			setInputValue("");
+		}
+	};
 
+	const deleteTasks = (id) => {
+		settodoList(todoList.filter((task, index) => index !== id));
+	};
 
 	return (
 		<div className="notes-body m-4 p-5 rounded bg-secondary">
-			<h1 className="text-center m-4">Pending Tasks 📝: <span>{count}</span> </h1>
+			<h1 className="text-center m-4">Pending Tasks 📝: <span>{todoList.length}</span></h1>
 
 			<div className="input-group mb-3">
-				<input type="text" class="form-control" placeholder="Press 'Enter' to save new note :)" aria-label="what" onKeyUp={addTask} value={inputValue.label} onChange={(e) => setInputValue(e.target.value)} />
-				{/* <button onClick={addTask} className="btn btn-success input-group-text"><i class="bi bi-plus-circle"></i></button> */}
+				<input
+					class="form-control"
+					type="text"
+					value={inputValue}
+					onChange={(e) => setInputValue(e.target.value)}
+					onKeyUp={addTask}
+					placeholder="Press 'Enter' to save new note :)"
+				/>
 			</div>
 
-
-			<ol className="list-group list-group-numbered pt-2 d-grid gap-2 d-flex">
-				{items.length == 0 ? (<li className="list-group-item gap-3 fs-5 rounded d-flex justify-content-between align-items-center">No tasks, add a task</li>) : (items.map((item, index) => (
-
-					<li className="list-group-item gap-3 fs-5 rounded d-flex justify-content-between align-items-center" key={index}>{item.label}<button className="btn btn-outline-danger ms-2 py-0 px-1" onClick={() => deleteItems(index)}>
-						<i className="bi bi-x-lg"></i>
-
-					</button>
-
-					</li>
-				)))}
-			</ol>
+			<ul className="list-group list-group-numbered pt-2 d-grid gap-2 d-flex">
+				{todoList.length === 0 ? (
+					<li className="list-group-item gap-3 fs-5 rounded d-flex justify-content-between align-items-center">No tasks, lucky!</li>
+				) : (
+					todoList.map((task, index) => (
+						<li className="list-group-item gap-3 fs-5 rounded d-flex justify-content-between align-items-center" key={index}>
+							{task.label}{" "}
+							<button className="btn btn-outline-danger ms-2 py-0 px-1" type="button" onClick={() => deleteTasks(index)}>
+								<i class="bi bi-trash"></i>
+							</button>
+						</li>
+					))
+				)}
+			</ul>
 		</div>
 	);
 };
